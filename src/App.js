fa917f04f2bf53c0503ffb1eab12e3309fed3f31
janusha98 <49071176+janusha98@users.noreply.github.com>
2021-07-13 08:11:42 -0700

    final code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./styles.css";
 import { useState } from "react";
 import axios from "axios";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import { Sparklines, SparklinesLine } from "react-sparklines";
+import _ from "lodash";
 
 export default function App() {
   const [place, setPlace] = useState("");
   const [weatherContent, setWeatherContent] = useState({});
+  console.log(weatherContent);
   const getPlace = (e) => {
     setPlace(e.target.value);
   };
@@ -28,9 +31,9 @@ export default function App() {
       .get(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${latLong.lat}&lon=${latLong.lng}&appid=bfc7e3515b286d62d95bf81265bff46c`
       )
-      .then((response) => (weatherDetails = response.data))
+      .then((response) => response.data)
       .catch((e) => console.log(e));
-    return weatherDetails;
+    return setWeatherContent(weatherDetails);
   };
   return (
     <div className="App">
@@ -38,6 +41,16 @@ export default function App() {
       <h2>Edit to see some magic happen!</h2>
       <input type="text" onChange={getPlace} />
       <Button onClick={weatherData}>Click</Button>
+      {_.isEmpty(weatherContent)
+        ? null
+        : Object.keys(weatherContent).map((data) => (
+            <Card>
+              <Card.Body>
+                <p>{data.daily[0].rain}</p>
+                <p>{data.daily[0].temp}</p>
+              </Card.Body>
+            </Card>
+          ))}
     </div>
   );
 }
