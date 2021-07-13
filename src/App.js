e811ae442235a127a4acb6e6e491d54f01b45eae
janusha98 <49071176+janusha98@users.noreply.github.com>
2021-07-13 05:01:31 -0700

    API Calls

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import "./styles.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
+import { Button } from "react-bootstrap";
 
 export default function App() {
+  const [place, setPlace] = useState("");
+  const [weatherContent, setWeatherContent] = useState({});
+  const getPlace = (e) => {
+    setPlace(e.target.value);
+  };
+  const googleApi = async (placeName) => {
+    let latLong,
+      place = placeName;
+    latLong = await axios
+      .get(
+        `https://api.opencagedata.com/geocode/v1/json?q=${place}&key=a3d9db842e7241ebbbf07e0a219e02f8`
+      )
+      .then((response) => response.data.results[0].geometry)
+      .catch((e) => console.log(e));
+    return latLong;
+  };
+
+  const weatherData = async () => {
+    const latLong = await googleApi(place);
+    let weatherDetails;
+    weatherDetails = await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${latLong.lat}&lon=${latLong.lng}&appid=bfc7e3515b286d62d95bf81265bff46c`
+      )
+      .then((response) => setWeatherContent(response.data))
+      .catch((e) => console.log(e));
+    return <DisplayWeatherContent {...weatherContent} />;
+  };
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Edit to see some magic happen!</h2>
-      <button>Click</button>
+      <input type="text" onChange={getPlace} />
+      <Button onClick={weatherData}>Click</Button>
     </div>
   );
 }
